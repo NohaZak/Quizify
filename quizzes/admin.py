a771971f383b7ad32ff4b7d9d2548afc9c424882
NohaZak <nohazakaria55@gmail.com>
2024-12-26 17:16:56 -0500

    Add .gitattributes to normalize line endings

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Quiz, Question, Result
+from .models import Quiz, Question, Choice, Result
 
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')  # Display Choice text, associated Question, and correctness
+    list_filter = ('question', 'is_correct')  # Allow filtering by Question and correctness
+    search_fields = ('text', 'question__text')  # Searchable fields (text and related question text)
+    autocomplete_fields = ('question',)  # Autocomplete for Question ForeignKey
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ('text',)  # Make sure Question model is searchable
+
+
+# Register models
 admin.site.register(Quiz)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Result)
-
